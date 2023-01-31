d14609ecdf932a1d09e9da923b7b3f77f2c2e17d
Linet-001 <linetmuchunu@gmail.com>
2023-01-31 23:33:17 +0300

    my task

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,25 +1,34 @@
-/*
- * File: 4-update_grade_by_city.js
- * Auth: linet
- */
+function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  if (!Array.isArray(newGrades)) {
+    return [];
+  }
 
-/**
- * updateStudentGradeByCity - Retrieves a list of students by city with their grades.
- * @studentList: A list of students.
- * @city: Student's location.
- * @newGrades:Student's grade.
- * Return: An array of students for a specific city with their new grade.
- */
-export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  return studentList
-    .filter((item) => item.location === city)
-    .map((student) => {
-      const grades = newGrades.filter((item) => item.studentId === student.id);
-      const grade = grades.length ? grades[0].grade : 'N/A';
-      return {
-        ...student,
-        grade,
-      };
-    });
+  const stCity = students.filter((student) => student.location === city);
+
+  const studentsGraded = stCity.map((student) => {
+    const gradeFilter = newGrades.filter(
+      (newGrade) => newGrade.studentId === student.id,
+    );
+
+    let grade;
+
+    if (gradeFilter[0]) {
+      grade = gradeFilter[0].grade;
+    } else {
+      grade = 'N/A';
+    }
+
+    return {
+      ...student,
+      grade,
+    };
+  });
+
+  return studentsGraded;
 }
+
+export default updateStudentGradeByCity;
 
